b4b8aa3c2be333dffe3349ebc29dceab61919dce
Victoria Knyazeva <v.b.knyazeva@gmail.com>
2024-05-04 14:33:00 +0700

    initial

---
+++ src/main/java/ru/victoriaknyazeva/otus/webserver/Dispatcher.java
@@ -0,0 +1,31 @@
+package ru.victoriaknyazeva.otus.webserver;
+
+import ru.victoriaknyazeva.otus.webserver.processors.CalculatorRequestProcessor;
+import ru.victoriaknyazeva.otus.webserver.processors.HelloWorldRequestProcessor;
+import ru.victoriaknyazeva.otus.webserver.processors.RequestProcessor;
+import ru.victoriaknyazeva.otus.webserver.processors.UnknownOperationRequestProcessor;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Dispatcher {
+    private Map<String, RequestProcessor> router;
+    private RequestProcessor unknownOperationRequestProcessor;
+
+    public Dispatcher() {
+        this.router = new HashMap<>();
+        this.router.put("/calc", new CalculatorRequestProcessor());
+        this.router.put("/hello", new HelloWorldRequestProcessor());
+        this.unknownOperationRequestProcessor = new UnknownOperationRequestProcessor();
+    }
+
+    public void execute(HttpRequest httpRequest, OutputStream outputStream) throws IOException {
+        if (!router.containsKey(httpRequest.getUri())) {
+            unknownOperationRequestProcessor.execute(httpRequest, outputStream);
+            return;
+        }
+        router.get(httpRequest.getUri()).execute(httpRequest, outputStream);
+    }
+}
---
+++ src/main/java/ru/victoriaknyazeva/otus/webserver/HttpMethod.java
@@ -0,0 +1,5 @@
+package ru.victoriaknyazeva.otus.webserver;
+
+public enum HttpMethod {
+    GET, POST, PUT, DELETE
+}
