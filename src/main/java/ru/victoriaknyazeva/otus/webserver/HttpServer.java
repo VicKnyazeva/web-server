cca33b4553c744751b19f312a1460056ffbdd098
Victoria Knyazeva <v.b.knyazeva@gmail.com>
2024-05-06 23:46:13 +0700

    разбор заголовка запроса вынесен в отдельный метод

--- src/main/java/ru/victoriaknyazeva/otus/webserver/HttpServer.java
+++ src/main/java/ru/victoriaknyazeva/otus/webserver/HttpServer.java
@@ -31,13 +31,8 @@ public class HttpServer {
                     try (Socket ignored = socket;
                          OutputStream out = socket.getOutputStream();
                          InputStream in = socket.getInputStream()) {
-                        byte[] buffer = new byte[8192];
-                        int n = in.read(buffer);
-                        if (n > 0) {
-                            String rawRequest = new String(buffer, 0, n);
-                            HttpRequest request = new HttpRequest(rawRequest);
-                            request.info(true);
-
+                        HttpRequest request = getRequest(in);
+                        if (request != null) {
                             dispatcher.execute(request, out);
                         }
                     } catch (IOException e) {
@@ -48,5 +43,17 @@ public class HttpServer {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private HttpRequest getRequest(InputStream in) throws IOException {
+        byte[] buffer = new byte[8192];
+        int n = in.read(buffer);
+        if (n > 0) {
+            String rawRequest = new String(buffer, 0, n);
+            HttpRequest request = new HttpRequest(rawRequest);
+            request.info(true);
+            return request;
+        }
+        return null;
     }
 }
