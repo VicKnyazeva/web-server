47611a10deaa5209b91bcd14e274c185107126a8
Victoria Knyazeva <v.b.knyazeva@gmail.com>
2024-05-06 23:40:03 +0700

    добавлена обработка множества запросов

--- src/main/java/ru/victoriaknyazeva/otus/webserver/HttpServer.java
+++ src/main/java/ru/victoriaknyazeva/otus/webserver/HttpServer.java
@@ -3,35 +3,47 @@ package ru.victoriaknyazeva.otus.webserver;
 import ru.victoriaknyazeva.otus.webserver.application.Storage;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class HttpServer {
-    private int port;
-    private Dispatcher dispatcher;
+    private final int port;
+    private final ExecutorService executorService;
 
     public HttpServer(int port) {
         this.port = port;
+        this.executorService = Executors.newCachedThreadPool(); // сервис-исполнитель обработчиков запросов
     }
 
     public void start() {
         try (ServerSocket serverSocket = new ServerSocket(port)) {
             System.out.println("Сервер запущен на порту: " + port);
-            this.dispatcher = new Dispatcher();
+            Dispatcher dispatcher = new Dispatcher();
             System.out.println("Диспетчер проинициализирован");
             Storage.init();
             while (true) {
-                try (Socket socket = serverSocket.accept()) {
-                    byte[] buffer = new byte[8192];
-                    int n = socket.getInputStream().read(buffer);
-                    if (n > 0) {
-                        String rawRequest = new String(buffer, 0, n);
-                        HttpRequest request = new HttpRequest(rawRequest);
-                        request.info(true);
+                Socket socket = serverSocket.accept();
+                executorService.execute(() -> {
+                    try (Socket ignored = socket;
+                         OutputStream out = socket.getOutputStream();
+                         InputStream in = socket.getInputStream()) {
+                        byte[] buffer = new byte[8192];
+                        int n = in.read(buffer);
+                        if (n > 0) {
+                            String rawRequest = new String(buffer, 0, n);
+                            HttpRequest request = new HttpRequest(rawRequest);
+                            request.info(true);
 
-                        dispatcher.execute(request, socket.getOutputStream());
+                            dispatcher.execute(request, out);
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
                     }
-                }
+                });
             }
         } catch (IOException e) {
             e.printStackTrace();
