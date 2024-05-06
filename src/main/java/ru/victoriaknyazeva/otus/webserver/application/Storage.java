ccd2122578206e04d6620a3b0236b726596bfadf
Victoria Knyazeva <v.b.knyazeva@gmail.com>
2024-05-07 00:08:20 +0700

    добавлена возможность изменение продукта через метод PUT

--- src/main/java/ru/victoriaknyazeva/otus/webserver/application/Storage.java
+++ src/main/java/ru/victoriaknyazeva/otus/webserver/application/Storage.java
@@ -2,6 +2,7 @@ package ru.victoriaknyazeva.otus.webserver.application;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -16,7 +17,7 @@ public class Storage {
         items = new ArrayList<>();
 
         for (int i = 0; i < 3; i++) {
-            items.add(new Item("item " + i, 100 + (int)(Math.random() * 1000)));
+            items.add(new Item("item " + i, 100 + (int) (Math.random() * 1000)));
         }
     }
 
@@ -27,5 +28,24 @@ public class Storage {
     public static void save(Item item) {
         item.setId(UUID.randomUUID());
         items.add(item);
+    }
+
+    public static Item getById(UUID id) {
+        for (var item : items) {
+            if (item.getId().compareTo(id) == 0) {
+                return item;
+            }
+        }
+        return null;
+    }
+
+    public static boolean update(Item newItem) {
+        Item item = getById(newItem.getId());
+        if (item == null) {
+            return false;
+        }
+        item.setTitle(newItem.getTitle());
+        item.setPrice(newItem.getPrice());
+        return true;
     }
 }
